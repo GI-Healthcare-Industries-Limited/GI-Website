78be83027952090e7746409b41f705836d11eae9
Jake Callcut <jakecallcut123@gmail.com>
2024-09-02 14:31:28 +0100

    adding basic pages

---
+++ frontend/lib/providers/navigation_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/about_page.dart';
+import 'package:frontend/pages/contact_page.dart';
+import 'package:frontend/pages/home_page.dart';
+
+class NavigationProvider with ChangeNotifier {
+  int _currentIndex = 0;
+
+  // This list can be customized based on your app's pages
+  final List<Widget> _pages = [
+    HomePage(),
+    AboutPage(),
+    ContactPage(),
+  ];
+
+  // Get the current index
+  int get currentIndex => _currentIndex;
+
+  // Get the current page
+  Widget get currentPage => _pages[_currentIndex];
+
+  // Function to update the current index and notify listeners
+  void updateIndex(int newIndex) {
+    _currentIndex = newIndex;
+    notifyListeners();
+  }
+}

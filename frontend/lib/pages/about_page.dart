8a1e4fe5542b1c8d5634eaf85d44102c1d1e184d
Jake Callcut <jakecallcut123@gmail.com>
2024-09-07 16:08:03 +0100

    added mission and vision statement

--- frontend/lib/pages/about_page.dart
+++ frontend/lib/pages/about_page.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/widgets/background_video_page.dart';
-import 'package:frontend/widgets/footer.dart';
-import 'package:frontend/widgets/machine_map_section.dart';
+import 'package:frontend/themes/main_theme.dart';
 import 'package:frontend/widgets/navigation_bar.dart';
 import 'package:frontend/widgets/person_card.dart';
-import 'package:frontend/widgets/problem_section.dart';
-import 'package:frontend/widgets/solution_section.dart';
-import 'package:frontend/widgets/supporters.dart';
+import 'package:frontend/widgets/about_card.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -51,24 +47,68 @@ class _AboutPageState extends State<AboutPage> {
         children: [
           SingleChildScrollView(
             controller: _scrollController,
-            child: const Column(
-              children: [
-                Center(
-                  child: Padding(
+            child: const Center(
+              child: Column(
+                children: [
+                  Padding(
                     padding: EdgeInsets.fromLTRB(300, 200, 300, 60),
-                    child: Wrap(
+                    child: Column(
                       children: [
-                        PersonCard(imagePath: 'assets/images/ash.png', name: 'Aswath GI', jobRole: 'CEO'),
-                        PersonCard(imagePath: 'assets/images/adip.png', name: 'Adip Das', jobRole: 'CTO'),
-                        PersonCard(imagePath: 'assets/images/tuan.png', name: 'Tuan Nguyen', jobRole: 'Software Engineer'),
-                        PersonCard(imagePath: 'assets/images/chris.png', name: 'Chris Mitchell', jobRole: 'Robotics Engineer'),
-                        PersonCard(imagePath: 'assets/images/jake.png', name: 'Jake Callcut', jobRole: 'Software Developer'),
-                        PersonCard(imagePath: 'assets/images/bailey.png', name: 'Bailey Tuddenham', jobRole: 'Design Engineer'),
+                        Text('About Us', style: MainTheme.problemText,),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(200, 120, 200, 16),
+                          child: AboutCard(
+                            title: 'Vision',
+                            text:
+                                'Lorem ipsum odor amet, consectetuer adipiscing elit. Enim eleifend nec magna cubilia sagittis aptent auctor pellentesque. Ligula ad turpis etiam luctus eu ante, luctus pellentesque. Duis sapien habitant molestie commodo praesent velit scelerisque habitasse. Mollis ridiculus a aenean efficitur curae sodales ligula. Ex netus sociosqu himenaeos vivamus ante quam congue dignissim. Vulputate sapien magna conubia congue praesent urna. Adipiscing curae magnis sem platea nec vivamus. Aliquet tristique semper tempor lacinia placerat montes condimentum.',
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(200, 16, 200, 16),
+                          child: AboutCard(
+                            title: 'Mission',
+                            text:
+                                'Lorem ipsum odor amet, consectetuer adipiscing elit. Enim eleifend nec magna cubilia sagittis aptent auctor pellentesque. Ligula ad turpis etiam luctus eu ante, luctus pellentesque. Duis sapien habitant molestie commodo praesent velit scelerisque habitasse. Mollis ridiculus a aenean efficitur curae sodales ligula. Ex netus sociosqu himenaeos vivamus ante quam congue dignissim. Vulputate sapien magna conubia congue praesent urna. Adipiscing curae magnis sem platea nec vivamus. Aliquet tristique semper tempor lacinia placerat montes condimentum.',
+                          ),
+                        ),
+                        SizedBox(height: 200,),
+                        Text(
+                          'Meet the Team',
+                          style: MainTheme.problemText,
+                        ),
+                        Wrap(
+                          children: [
+                            PersonCard(
+                                imagePath: 'assets/images/ash.png',
+                                name: 'Aswath GI',
+                                jobRole: 'CEO'),
+                            PersonCard(
+                                imagePath: 'assets/images/adip.png',
+                                name: 'Adip Das',
+                                jobRole: 'CTO'),
+                            PersonCard(
+                                imagePath: 'assets/images/tuan.png',
+                                name: 'Tuan Nguyen',
+                                jobRole: 'Software Engineer'),
+                            PersonCard(
+                                imagePath: 'assets/images/chris.png',
+                                name: 'Chris Mitchell',
+                                jobRole: 'Robotics Engineer'),
+                            PersonCard(
+                                imagePath: 'assets/images/jake.png',
+                                name: 'Jake Callcut',
+                                jobRole: 'Software Developer'),
+                            PersonCard(
+                                imagePath: 'assets/images/bailey.png',
+                                name: 'Bailey Tuddenham',
+                                jobRole: 'Design Engineer'),
+                          ],
+                        ),
                       ],
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           const NavBar(isTransparent: false),
---
+++ frontend/lib/widgets/about_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AboutCard extends StatelessWidget {
+  const AboutCard({super.key, required this.title, required this.text});
+
+  final String title;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Image.asset('assets/images/$title.png', height: 60,),
+              const SizedBox(width: 8.0),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          Text(
+            text,
+            style: const TextStyle(fontSize: 14.0),
+          ),
+        ],
+      ),
+    );
+  }
+}

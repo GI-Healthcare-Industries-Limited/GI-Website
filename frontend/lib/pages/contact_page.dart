33360f30a99d45cfe3cf40143853ae558f4538ac
Jake Callcut <jakecallcut123@gmail.com>
2024-09-14 13:49:58 +0100

    added placeholders for empty pages

--- frontend/lib/pages/contact_page.dart
+++ frontend/lib/pages/contact_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/navigation_bar.dart';
 
-class ContactPage extends StatelessWidget{
-  const ContactPage({super.key});
+class CareersPage extends StatefulWidget {
+  const CareersPage({super.key});
+
+  @override
+  _CareersPageState createState() => _CareersPageState();
+}
+
+class _CareersPageState extends State<CareersPage> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isAtTop = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels == 0 && !_isAtTop) {
+      setState(() {
+        _isAtTop = true;
+      });
+    } else if (_scrollController.position.pixels > 0 && _isAtTop) {
+      setState(() {
+        _isAtTop = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
 
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            controller: _scrollController,
+            child: Container(
+              color: Colors.white,
+              child: const Text('Careers content coming soon'),
+            ),
+          ),
+          NavBar(isTransparent: _isAtTop),
+        ],
+      ),
+    );
+  }
 }

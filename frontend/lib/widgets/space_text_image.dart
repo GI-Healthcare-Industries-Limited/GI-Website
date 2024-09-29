933efa751fdbfd95ee22f7c897f4245d3151ff3f
Jake Callcut <jakecallcut123@gmail.com>
2024-09-29 15:52:12 +0100

    space food sections done

--- frontend/lib/widgets/space_text_image.dart
+++ frontend/lib/widgets/space_text_image.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SpaceTextImage extends StatelessWidget {
@@ -5,62 +7,98 @@ class SpaceTextImage extends StatelessWidget {
   final String text;
   final bool imageFirst;
 
-  SpaceTextImage({required this.imagePath, required this.text, required this.imageFirst});
+  SpaceTextImage(
+      {required this.imagePath, required this.text, required this.imageFirst});
   @override
   Widget build(BuildContext context) {
-    return imageFirst 
-    ? Padding(
-      padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            color: Colors.grey,
-            child: Image.asset(imagePath, fit: BoxFit.cover),
-          ),
-          SizedBox(width: 16),
-          Container(
-            width: 600,
-            padding: EdgeInsets.all(16),
-            child: Text(
-              text,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.justify,
+    return imageFirst
+
+        //image appearing before text
+        ? Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Image.asset(imagePath, fit: BoxFit.cover),
+                ),
+                ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                    child: Container(
+                      width: 600,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(12),
+                            bottomRight: Radius.circular(12)),
+                        border: Border(
+                          top: BorderSide(color: Colors.white),
+                          right: BorderSide(color: Colors.white),
+                          bottom: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(60),
+                        child: Text(
+                          text,
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-        ],
-      ),
-    )
-    : Padding(
-      padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          
-          SizedBox(width: 16),
-          Container(
-            width: 600,
-            padding: EdgeInsets.all(16),
-            child: Text(
-              text,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.justify,
+          )
+
+        //image appearing after text
+        : Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                    child: Container(
+                      width: 600,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(12),
+                            bottomLeft: Radius.circular(12)),
+                        border: Border(
+                          top: BorderSide(color: Colors.white),
+                          left: BorderSide(color: Colors.white),
+                          bottom: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(60),
+                        child: Text(
+                          text,
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Image.asset(imagePath, fit: BoxFit.cover),
+                ),
+              ],
             ),
-          ),
-          Container(
-            color: Colors.grey,
-            child: Image.asset(imagePath, fit: BoxFit.cover),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
